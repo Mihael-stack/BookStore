61581fedf28742c005c4fc06f8322847979d6c49
Mihael-stack <60133143+Mihael-stack@users.noreply.github.com>
2022-10-01 19:50:43 +0200

    getting ready for RestController, working on maven to output jar for tomcat and adding web.xml for REST Services

---
+++ src/main/java/com/mihael/bookStore/restcontrollers/CustomerRestController.java
@@ -0,0 +1,24 @@
+package com.mihael.bookStore.restcontrollers;
+
+import com.mihael.bookStore.entity.Customer;
+import com.mihael.bookStore.exceptions.CustomerNotFoundException;
+import com.mihael.bookStore.services.customer.CustomerManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomerRestController {
+    private final CustomerManagementService customerService;
+
+    @Autowired
+    public CustomerRestController(CustomerManagementService customerService){
+        this.customerService = customerService;
+    }
+
+    @RequestMapping("/customer/{id}")
+    public Customer findCustomerById(@PathVariable String id) throws CustomerNotFoundException {
+        return customerService.findCustomerById(Long.valueOf(id));
+    }
+}
